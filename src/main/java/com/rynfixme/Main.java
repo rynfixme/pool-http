deb28ab6962ea371a3aa30d187cb5c26a61ff60f
rynfixme <qyd4v8id@gmail.com>
2024-06-05 23:22:05 +0900

    feat: update extension

--- src/main/java/com/rynfixme/Main.java
+++ src/main/java/com/rynfixme/Main.java
@@ -10,7 +10,7 @@ import java.util.concurrent.LinkedBlockingDeque;
 public class Main implements BurpExtension {
 
     static final String DO_CAPTURE = "POOL_HTTP_SKIP_RECORD";
-    static final String STORE_DIR_NAME = "STORE_DIR_NAME";
+    static final String STORE_DIR_NAME = "POOL_HTTP_STORE_DIR_NAME";
     static final String STORE_FILE_NAME = "POOL_HTTP_STORE_FILE_NAME";
 
     @Override
@@ -20,8 +20,8 @@ public class Main implements BurpExtension {
         persist.setBoolean(DO_CAPTURE, true);
 
         PoolHttpFileUtil fileUtil = new PoolHttpFileUtil(persist);
-
         PoolHttpCapture capture = new PoolHttpCapture(persist, api.logging());
+
         api.http().registerHttpHandler(new PoolHttpHandler(capture, api.logging()));
         api.userInterface().registerSuiteTab("Pool HTTP", new PoolHttpGUI(persist, fileUtil, api.logging()));
     }
